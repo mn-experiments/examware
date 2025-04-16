f365ae8fd7d1714bb08a0d0d350ee82c3143b111
max-nazarati <53535394+max-nazarati@users.noreply.github.com>
2025-04-16 19:20:53 +0200

    created project

---
+++ src/test/java/examware/rest_validation/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package examware.rest_validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
